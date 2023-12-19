e15faa76881b77ea4ee13d46f338925a1a04ceb4
JuMaxi <jmaximovski@gmail.com>
2023-12-19 18:08:01 +0000

    Day08-Part01

---
+++ Adventoofcode2023/Day08/Part01.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventoofcode2023.Day08
+{
+    public class Part01
+    {
+        private string[] ReadFile()
+        {
+            //return File.ReadAllLines("C:/Dev/Adventoofcode2023/Adventoofcode2023/Day08/sample-day08.txt");
+            //return File.ReadAllLines("C:/Dev/Adventoofcode2023/Adventoofcode2023/Day08/sample2-day08.txt");
+            return File.ReadAllLines("C:/Dev/Adventoofcode2023/Adventoofcode2023/Day08/input-day08.txt");
+
+        }
+
+        private Dictionary<string, List<string>> SplitFile()
+        {
+            string[] input = ReadFile();
+
+            Dictionary<string, List<string>> nodes = new();
+
+            for (int line = 2; line < input.Length; line++)
+            {
+                string[] path = input[line].Split('=');
+                path[0] = path[0].Trim();
+                string[] nn = path[1].Split(",");
+
+                List<string> node = new()
+                {
+                    nn[0].Replace('(', ' ').Trim(),
+                    nn[1].Replace(')', ' ').Trim()
+                };
+
+                nodes.Add(path[0], node);
+            }
+            return nodes;
+        }
+
+        public long SumSteps()
+        {
+            string[] OriginalInput = ReadFile();
+            string directions = OriginalInput[0];
+
+            Dictionary<string, List<string>> nodes = SplitFile();
+            long sum = 0;
+
+            var key = "AAA";
+
+            for (int charDirections = 0; charDirections < directions.Length; charDirections++)
+            {
+                int whereToGo = 0;
+                if (directions[charDirections] == 'R') { whereToGo = 1; }
+
+                var nextNode = nodes[key][whereToGo];
+                key = nextNode;
+                sum++;
+
+                if(key == "ZZZ")
+                {
+                    break;
+                }
+                else
+                {
+                    if(charDirections == directions.Length - 1)
+                    {
+                        charDirections = -1;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Adventoofcode2023/Day08/UnitTestDay08.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventoofcode2023.Day08
+{
+    public class UnitTestDay08
+    {
+        [Fact]
+        public void Test1()
+        {
+            Part01 part01 = new();
+
+            long Steps = part01.SumSteps();
+
+            //Assert.Equal(2, Steps);
+            Assert.Equal(6, Steps);
+
+        }
+    }
+}
